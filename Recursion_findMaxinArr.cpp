c5c1e3a0c100c33aefdf8dca77772bfd1c5e07f1
Jayachitra Sundaram <jayachitra.b.e@gmail.com>
2026-09-16 12:51:48 +0530

    Update Recursion_findMaxinArr.cpp

--- Recursion_findMaxinArr.cpp
+++ Recursion_findMaxinArr.cpp
@@ -2,7 +2,7 @@
 
 int maximum (int *p, int index)
 {
-    int max = 0;
+    int max =0;
     if(index < 0) return max;
 
     max = maximum (p, index -1);
@@ -13,9 +13,21 @@ int maximum (int *p, int index)
     return max;
 
 }
+
+void maximum (int *p, int index, int& max)
+{
+    if(index < 0) return;
+    if(*(p + index) > max) // p points to arr[0] , so add index to get the number from the index location
+        max = *(p+index) ;
+    maximum (p, index -1, max);
+}
 int main()
 {
+    int size = 10;
     int arr[10] = {12, 34, 12, 542, 45, 87, 5, 2, 1, 8};
-    std::cout << maximum(&arr, 9);
+    std::cout << maximum(arr, size-1) << std::endl;
+    int max = 0;
+    maximum (arr, size-1, max);
+    std:: cout << "Maximum :" << max << std::endl;
     return 0;
 }
